e159c65f15069655d633591b0d829b33e990a758
root <call-me-prof>
2026-04-28 08:06:41 +0000

    feat: implement binary_tree_delete function

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: A pointer to the root node of the tree to delete
+ *
+ * Description: If tree is NULL, do nothing.
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	/* Delete left subtree recursively */
+	binary_tree_delete(tree->left);
+
+	/* Delete right subtree recursively */
+	binary_tree_delete(tree->right);
+
+	/* Free the current node */
+	free(tree);
+}
