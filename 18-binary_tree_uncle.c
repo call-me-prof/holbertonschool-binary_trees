95f351e44857dcf67f568797b8bcddfaae84f420
SaadTAr <saadalatar3445@gmail.com>
2026-05-09 01:19:15 +0000

    Fix binary tree uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,9 +8,17 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *parent, *grandparent;
+
 	if (node == NULL || node->parent == NULL ||
 	    node->parent->parent == NULL)
 		return (NULL);
 
-	return (binary_tree_sibling(node->parent));
+	parent = node->parent;
+	grandparent = parent->parent;
+
+	if (grandparent->left == parent)
+		return (grandparent->right);
+
+	return (grandparent->left);
 }
